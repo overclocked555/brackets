8f531dad2c185f168854693ab2230e315b4603e1
Alexandr Khokhanov <hohanov@gmail.com>
2020-10-02 21:39:21 +0300

    feat: solve brackets task

--- src/index.js
+++ src/index.js
@@ -1,30 +1,12 @@
-function noOfOpenBracket(char, bracketsConfig) {
-    for (i = 0; i < bracketsConfig.length; i++) {
-        if (char === bracketsConfig[i][0]) return i;
-    }
-    return undefined;
-}
-function noOfCloseBracket(char, bracketsConfig) {
-    for (i = 0; i < bracketsConfig.length; i++) {
-        if (char === bracketsConfig[i][1]) return i;
-    }
-    return undefined;
-}
-
 module.exports = function check(str, bracketsConfig) {
-    queue = [];
-    for (let char of str) {
-        noOfCurrentOpenBracket = noOfOpenBracket(char, bracketsConfig);
-        noOfCurrentCloseBracket = noOfCloseBracket(char, bracketsConfig);
-        //console.log(char, noOfCurrentOpenBracket, noOfCurrentCloseBracket);
-        // если не скобка - пропускаем символ
-        if (noOfCurrentOpenBracket === undefined && noOfCurrentCloseBracket === undefined) continue;
-        //если открывающая скобка - запишем в массив ее номер
-        if (noOfCurrentOpenBracket !== undefined) queue.push(noOfCurrentOpenBracket)         
-        //если закрывающая, соотвествующая последней открытой - удалим из массива
-        else {
-            if (queue.pop() !== noOfCurrentCloseBracket) return false;
-        }
+    let bracketsStr = bracketsConfig.map( (item) => item.join("") );
+    i = 0;
+    while (i < bracketsStr.length) {
+      while(str.includes(bracketsStr[i]) ) {
+        str = str.replace(bracketsStr[i], "");
+        i = -1;
       }
-    return queue.length === 0;
-}
+      i++;
+    }
+    return (str) ? false : true;
+  }
