e3d54e0f655d178ea078e17b0f34c24440ca0f30
Alexandr Khokhanov <hohanov@gmail.com>
2020-10-02 21:04:30 +0300

    feat: solve brackets task (without |)

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
+function noOfOpenBracket(char, bracketsConfig) {
+    for (i = 0; i < bracketsConfig.length; i++) {
+        if (char === bracketsConfig[i][0]) return i;
+    }
+    return undefined;
+}
+function noOfCloseBracket(char, bracketsConfig) {
+    for (i = 0; i < bracketsConfig.length; i++) {
+        if (char === bracketsConfig[i][1]) return i;
+    }
+    return undefined;
+}
+
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    queue = [];
+    for (let char of str) {
+        noOfCurrentOpenBracket = noOfOpenBracket(char, bracketsConfig);
+        noOfCurrentCloseBracket = noOfCloseBracket(char, bracketsConfig);
+        //console.log(char, noOfCurrentOpenBracket, noOfCurrentCloseBracket);
+        // если не скобка - пропускаем символ
+        if (noOfCurrentOpenBracket === undefined && noOfCurrentCloseBracket === undefined) continue;
+        //если открывающая скобка - запишем в массив ее номер
+        if (noOfCurrentOpenBracket !== undefined) queue.push(noOfCurrentOpenBracket)         
+        //если закрывающая, соотвествующая последней открытой - удалим из массива
+        else {
+            if (queue.pop() !== noOfCurrentCloseBracket) return false;
+        }
+      }
+    return queue.length === 0;
 }
